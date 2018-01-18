95233c0ba77361a5c3d03adc0619d48d8e766069
Duke Leto <duke@leto.net>
2018-01-18 20:13:13 +0000

    Parse taddrs from P2PKH and P2SH, start working on P2MS+P2PK

--- utxo/util.py
+++ utxo/util.py
@@ -1,6 +1,8 @@
 import hashlib
 import os
 import struct
+import base58
+import secp256k1
 
 from binascii import hexlify
 from utxo.b128 import decompress_amount
@@ -24,8 +26,46 @@ def read_utxo_file(f):
         script = f.read(sz)
 
         assert f.read(1) == '\n'
-        amount =  "%d.%08d" %  (amt / 100000000 , amt % 100000000 )
-        print("{},{}".format(amount, hexlify(script)))
+        amount     =  "%d.%08d" %  (amt / 100000000 , amt % 100000000 )
+        data       = hexlify(script)
+        if data[0:4]==b'76a9':
+            # P2PKH
+            num_bytes  = script[2]
+            public_key = script[3:23]
+            z          = b'\00'+public_key
+            z          = base58.b58encode_check(z)
+            print("{},{},{}".format(amount, z, hexlify(script)))
+        elif data[0:2] == b'a9':
+            # P2SH
+            num_bytes  = script[1]
+            public_key = script[2:22]
+            z          = b'\05'+public_key
+            z          = base58.b58encode_check(z)
+            print("{},{},{}".format(amount, z, hexlify(script)))
+        elif data[0:2] == b'51' and data[-2:] == b'ae':
+            # CHECKMULTISIG
+            public_key = script[2:22]
+            z          = b'\05'+public_key
+            z          = base58.b58encode_check(z)
+            #print("multisig: {},{},{}".format(amount, z, hexlify(script)))
+            print("{},,{}".format(amount, hexlify(script)))
+        elif data[-2:] == b'ac':
+            # P2PK 
+            #assert len(data) == 32
+            #comp_pubkey = chr(5) + data[:-2]
+            #print comp_pubkey, len(comp_pubkey)
+            #pubkey = secp256k1.PublicKey(
+            #    comp_pubkey, raw=True
+            #).serialize(compressed=False)
+
+            # TODO: proper prefix?
+            #z          = b'\99'+public_key
+            #z          = base58.b58encode_check(z)
+            #print("p2pk: {},{},{}".format(amount, z, hexlify(script)))
+            print("{},,{}".format(amount, hexlify(script)))
+        else:
+            print("{},,{}".format(amount, hexlify(script)))
+
         head = f.read(16)
 
 
